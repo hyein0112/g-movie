4e004e9b674a96a273d2a8d3f2604c3ba79eef7d
hyein0112 <bhi12134@gmail.com>
2023-05-25 08:38:57 +0900

    Signup Page 화면 구성

--- src/components/pages/SignupPage/index.tsx
+++ src/components/pages/SignupPage/index.tsx
@@ -1,10 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
+
 import * as S from "./style";
+import * as I from "../../../assets";
 
 const SignupPage = () => {
   return (
-    <>
-      <span>회원가입</span>
-    </>
+    <S.Container>
+      <I.Logo />
+      <S.InputBox>
+        <S.Input spellCheck="false" placeholder="이름을 입력하세요." />
+        <S.Input spellCheck="false" placeholder="이메일을 입력하세요." />
+        <S.Input spellCheck="false" placeholder="비밀번호를 입력하세요." />
+        <S.Input spellCheck="false" placeholder="비밀번호를 재입력하세요." />
+      </S.InputBox>
+      <Link to="/login">
+        <S.SignupButton>회원가입</S.SignupButton>
+      </Link>
+    </S.Container>
   );
 };
 
--- src/components/pages/SignupPage/style.ts
+++ src/components/pages/SignupPage/style.ts
@@ -1,1 +1,46 @@
 import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  padding-top: 23.15vh;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 6.4vh 0 10.47vh 0;
+`;
+
+export const Input = styled.input`
+  width: 250px;
+  height: 15px;
+  border: 0;
+  border-bottom: 1px solid #b7c7e8;
+  background: none;
+  font-size: 12px;
+  padding: 32px 0 7px 0;
+
+  &:focus {
+    border-bottom: 1px solid #6886c5;
+    outline: none;
+  }
+`;
+
+export const SignupButton = styled.button`
+  background: #6886c5;
+  border-radius: 8px;
+  border: none;
+  width: 250px;
+  height: 38px;
+  color: #fff;
+  font-weight: 600;
+  font-size: 14px;
+  cursor: pointer;
+
+  &:active {
+    filter: brightness(90%);
+  }
+`;
