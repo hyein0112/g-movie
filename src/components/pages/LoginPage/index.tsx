673c5abcc1d423b0a637115c975a57d0bc2b0c04
hyein0112 <bhi12134@gmail.com>
2023-05-26 16:07:35 +0900

    login 기능 구현

--- src/components/pages/LoginPage/index.tsx
+++ src/components/pages/LoginPage/index.tsx
@@ -1,20 +1,44 @@
 import { Link } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 import * as S from "./style";
 import * as I from "../../../assets";
+import axios from "axios";
 
 const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  const handleSubmitData = async (data: any) => {
+    console.log(data);
+    try {
+      await axios.post("/api/user/signin", {
+        ...data,
+      });
+      window.location.pathname = "/";
+    } catch (e: any) {
+      console.log(e);
+      e?.response.status === 500 &&
+        alert("아이디 및 비밀번호가 올바르지 않습니다.");
+    }
+  };
   return (
     <S.Container>
       <I.Logo />
-      <S.InputBox>
-        <S.Input spellCheck="false" placeholder="이메일을 입력하세요." />
-        <S.Input spellCheck="false" placeholder="비밀번호를 입력하세요." />
-        <S.ForgetPW>앗! 비밀번호를 잊으셨나요?</S.ForgetPW>
-      </S.InputBox>
-      <Link to="/">
+      <form onSubmit={handleSubmit(handleSubmitData)}>
+        <S.InputBox>
+          <S.Input
+            spellCheck="false"
+            placeholder="아이디를 입력하세요."
+            {...register("id")}
+          />
+          <S.Input
+            spellCheck="false"
+            placeholder="비밀번호를 입력하세요."
+            {...register("password")}
+          />
+          <S.ForgetPW>앗! 비밀번호를 잊으셨나요?</S.ForgetPW>
+        </S.InputBox>
         <S.LoginButton>로그인</S.LoginButton>
-      </Link>
+      </form>
       <Link to="/signup" style={{ color: "#9a9a9a" }}>
         <S.MoveSignupButton>회원가입</S.MoveSignupButton>
       </Link>
