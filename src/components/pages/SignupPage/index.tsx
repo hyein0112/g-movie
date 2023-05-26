6626b558cf28e5a411dcb02c2713e26562cbf1f3
hyein0112 <bhi12134@gmail.com>
2023-05-26 15:52:49 +0900

    회원가입 form 데이터 post

--- src/components/pages/SignupPage/index.tsx
+++ src/components/pages/SignupPage/index.tsx
@@ -1,21 +1,48 @@
 import { Link } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 import * as S from "./style";
 import * as I from "../../../assets";
+import axios, { AxiosResponse } from "axios";
 
 const SignupPage = () => {
+  const { register, handleSubmit } = useForm();
+  const customAxios = axios.create({});
+
+  const handleSubmitData = async (data: any) => {
+    console.log(data);
+    try {
+      await customAxios.post("/api/user/signup", {
+        ...data,
+      });
+      window.location = "/login";
+    } catch (e: any) {
+      e?.response.status === 500 && alert("이미 가입된 회원입니다.");
+    }
+  };
+
   return (
     <S.Container>
       <I.Logo />
-      <S.InputBox>
-        <S.Input spellCheck="false" placeholder="이름을 입력하세요." />
-        <S.Input spellCheck="false" placeholder="이메일을 입력하세요." />
-        <S.Input spellCheck="false" placeholder="비밀번호를 입력하세요." />
-        <S.Input spellCheck="false" placeholder="비밀번호를 재입력하세요." />
-      </S.InputBox>
-      <Link to="/login">
+      <form onSubmit={handleSubmit(handleSubmitData)}>
+        <S.InputBox>
+          <S.Input
+            spellCheck="false"
+            placeholder="이름을 입력하세요."
+            {...register("name")}
+          />
+          <S.Input
+            spellCheck="false"
+            placeholder="아이디를 입력하세요."
+            {...register("id")}
+          />
+          <S.Input
+            spellCheck="false"
+            placeholder="비밀번호를 입력하세요."
+            {...register("password")}
+          />
+        </S.InputBox>
         <S.SignupButton>회원가입</S.SignupButton>
-      </Link>
+      </form>
     </S.Container>
   );
 };
