50cb819a7b602823ce94787101405e324143f5bb
hyein0112 <bhi12134@gmail.com>
2023-05-26 23:41:07 +0900

    MovieBox

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -2,14 +2,16 @@ import * as S from "./style";
 import * as I from "../../../assets";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const MainPage = () => {
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function getMovie() {
       try {
         const data = await axios.get("/api/movie");
         console.log(data);
+        setMovies(data.data);
       } catch (e) {
         console.log(e);
       }
@@ -25,6 +27,14 @@ const MainPage = () => {
           <I.User />
         </Link>
       </S.UserButton>
+      <S.MovieBox>
+        {movies?.map((movie) => (
+          <S.MovieContainer key={movie.id}>
+            <S.MoviePoster src={movie.posterImg} />
+            <S.MovieName>{movie.title}</S.MovieName>
+          </S.MovieContainer>
+        ))}
+      </S.MovieBox>
     </S.Container>
   );
 };
--- src/components/pages/MainPage/style.ts
+++ src/components/pages/MainPage/style.ts
@@ -14,3 +14,35 @@ export const UserButton = styled.button`
   background: none;
   border: none;
 `;
+
+export const MovieBox = styled.div`
+  width: 95vw;
+  margin: 50px auto 0 auto;
+  display: grid;
+  grid-template-columns: 90px 90px 90px;
+  column-gap: 3vw;
+  row-gap: 2vh;
+  justify-content: center;
+`;
+
+export const MovieContainer = styled.div`
+  width: 90px;
+  displey: flex;
+  flex-direction: column;
+`;
+
+export const MoviePoster = styled.img`
+  min-width: 90px;
+  width: 90px;
+  height: 126px;
+  background: #d9d9d9;
+  border-radius: 5px;
+`;
+
+export const MovieName = styled.div`
+  width: 90px;
+  text-align: center;
+  font-size: 10px;
+  font-weight: 600;
+  padding-top: 5px;
+`;
