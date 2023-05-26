973187a5dc56d88b5b763ae839531e618c0979de
hyein0112 <bhi12134@gmail.com>
2023-05-26 15:05:13 +0900

    proxy 설정 및 영화 정보 가져오기

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,8 +1,22 @@
 import * as S from "./style";
 import * as I from "../../../assets";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
 const MainPage = () => {
+  useEffect(() => {
+    async function getMovie() {
+      try {
+        const data = await axios.get("/api/movie");
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getMovie();
+  }, []);
+
   return (
     <S.Container>
       <I.Logo />
