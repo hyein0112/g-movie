e07ba1793c40bd4f47ed0ef64f1ec93a3375d96e
hyein0112 <bhi12134@gmail.com>
2023-05-30 14:30:48 +0900

    불필요한 코드 삭제

--- src/components/LikeButton/index.tsx
+++ src/components/LikeButton/index.tsx
@@ -10,21 +10,16 @@ const LikeButton = ({ movieID, likeMovies }) => {
     likeMovies.map((movie) => {
       movie.id == movieID ? setLiked(true) : setLiked((prev) => prev);
     });
-    console.log(isLiked);
   }, [likeMovies, movieID]);
 
   const onClickLiked = async () => {
     try {
-      const data = await axios.post(`/api/movie/likes`, {
+      await axios.post(`/api/movie/likes`, {
         movieId: movieID,
         userId: uuid,
         isChecked: !isLiked,
       });
       setLiked((prev) => !prev);
-      console.log(data);
-      console.log(
-        `"movieId:" ${movieID} "userId:" ${uuid} "isChecked:" ${!isLiked}`
-      );
     } catch (e) {
       console.log(e);
     }
